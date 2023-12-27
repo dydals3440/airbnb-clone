3e8c57f71dfb67ecb26c79725c79c2d8bc92b3e3
Matthew <dydals3440@gmail.com>
2023-12-27 23:26:40 +0900

    feat: Favorite Page Screen

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client';
 
-import { AiOutlineMenu } from "react-icons/ai";
-import Avatar from "../Avatar";
-import { useCallback, useState } from "react";
-import MenuItem from "./MenuItem";
-import useRegisterModal from "@/app/hooks/useRegisterModal";
-import LoginModal from "../modals/LoginModal";
-import useLoginModal from "@/app/hooks/useLoginModal";
-import { SafeUser } from "@/app/types";
-import { signOut } from "next-auth/react";
-import useRentModal from "@/app/hooks/useRentModal";
-import { useRouter } from "next/navigation";
+import { AiOutlineMenu } from 'react-icons/ai';
+import Avatar from '../Avatar';
+import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import LoginModal from '../modals/LoginModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import { SafeUser } from '@/app/types';
+import { signOut } from 'next-auth/react';
+import useRentModal from '@/app/hooks/useRentModal';
+import { useRouter } from 'next/navigation';
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
@@ -37,44 +37,47 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
   }, [loginModal, currentUser, rentModal]);
 
   return (
-    <div className="relative">
-      <div className="flex flex-row items-center gap-3">
+    <div className='relative'>
+      <div className='flex flex-row items-center gap-3'>
         <div
           onClick={onRent}
-          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
+          className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
         >
           Airbnb your home
         </div>
         <div
           onClick={toggleOpen}
-          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+          className='p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'
         >
           <AiOutlineMenu />
-          <div className="hidden md:block">
+          <div className='hidden md:block'>
             <Avatar src={currentUser?.image} />
           </div>
         </div>
       </div>
       {isOpen && (
-        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
-          <div className="flex flex-col cursor-pointer">
+        <div className='absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
+          <div className='flex flex-col cursor-pointer'>
             {currentUser ? (
               <>
                 <MenuItem
-                  onClick={() => router.push("/trips")}
-                  label="My trips"
+                  onClick={() => router.push('/trips')}
+                  label='My trips'
                 />
-                <MenuItem onClick={() => {}} label="My favorites" />
-                <MenuItem onClick={() => {}} label="My reservations" />
-                <MenuItem onClick={() => {}} label="My properties" />
-                <MenuItem onClick={rentModal.onOpen} label="Airbnb my home" />
+                <MenuItem onClick={() => {}} label='My favorites' />
+                <MenuItem
+                  onClick={() => router.push('/reservations')}
+                  label='My reservations'
+                />
+                <MenuItem onClick={() => {}} label='My properties' />
+                <MenuItem onClick={rentModal.onOpen} label='Airbnb my home' />
                 <hr />
-                <MenuItem onClick={() => signOut()} label="Logout" />
+                <MenuItem onClick={() => signOut()} label='Logout' />
               </>
             ) : (
               <>
-                <MenuItem onClick={loginModal.onOpen} label="Login" />
-                <MenuItem onClick={registerModal.onOpen} label="Sign up" />
+                <MenuItem onClick={loginModal.onOpen} label='Login' />
+                <MenuItem onClick={registerModal.onOpen} label='Sign up' />
               </>
             )}
           </div>
